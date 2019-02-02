a18b6b67fc97fa60d7433c7e7e751e6d80359ee3
Ronak Patel <45703227+ronnie21@users.noreply.github.com>
2019-02-02 12:30:38 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+﻿$(window).load(function(){
+	$('#div-load').fadeOut(2000);
+});
+	
+$(function(){ 
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('#pageTop').fadeIn();
+		} else {
+			$('#pageTop').fadeOut();
+		}
+	}); 
+});
+
+$(function() {
+    $('#pageTop').bind('click',function(event){
+        var $anchor = $(this);
+        
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 900,'easeInOutExpo');
+		
+        event.preventDefault();
+    });
+});
+
+//slider
+$(function() {
+	$(".rslides").responsiveSlides();
+});
